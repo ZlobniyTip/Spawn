e39741608f1c26931693fc1b33c85265f253fa89
ZlobniyTip <4opick_97@mail.ru>
2023-11-09 18:21:22 +0300

    corrected

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,27 +7,20 @@ public class Spawner : MonoBehaviour
     [SerializeField] private Transform _target;
     [SerializeField] private Enemy _enemy;
 
-    private Coroutine _spawn;
-
     private void Start()
     {
-        _spawn = StartCoroutine(Spawn());
+        StartCoroutine(Spawn());
     }
 
     private IEnumerator Spawn()
     {
         var delayBeforeSpawn = new WaitForSeconds(6);
 
-        var enemy = Instantiate(_enemy, transform.position, Quaternion.identity);
-        enemy.SetDirection(_target);
-        yield return delayBeforeSpawn;
-
-        RestartCoroutine();
-    }
-
-    private void RestartCoroutine()
-    {
-        StopCoroutine(Spawn());
-        _spawn = StartCoroutine(Spawn());
+        while (true)
+        {
+            var enemy = Instantiate(_enemy, transform.position, Quaternion.identity);
+            enemy.SetDirection(_target);
+            yield return delayBeforeSpawn;
+        }
     }
 }
